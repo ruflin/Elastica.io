1858e4f77860fe88db431828d51bedf3cd411d91
ruflin <spam@ruflin.com>
2013-05-20 21:32:13 +0200

    Changes for Elastica gh-pages

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/Elastica/"
+http_images_path = "/Elastica/images"
+http_fonts_path = "/Elastica/fonts"
+css_dir = "public/Elastica/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
